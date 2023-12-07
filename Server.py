c2914d0ff2bb44171867f91632de10fbb91896a2
JunHeeKim28 <144142365+JunHeeKim28@users.noreply.github.com>
2023-12-08 02:44:50 +0900

    Update Server.py

--- Server.py
+++ Server.py
@@ -4,18 +4,7 @@ import RPi.GPIO as GPIO
 import time
 import pygame
 GPIO.setwarnings(False)
-
-def get_ip_address():
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(("8.8.8.8", 80))
-        ip = s.getsockname()[0]
-        s.close()
-        return ip
-    except Exception as e:
-        return "ERROR" + str(e)
-    
-    
+      
 app = Flask(__name__)
 
 @app.route('/')
