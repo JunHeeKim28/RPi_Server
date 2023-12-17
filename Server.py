5f925a8287aa4079a67d1e1a9b5388554160d4f7
JunHeeKim28 <144142365+JunHeeKim28@users.noreply.github.com>
2023-12-18 03:58:05 +0900

    Update Driver_Server.py

--- Server.py
+++ Server.py
@@ -2,40 +2,53 @@ from flask import Flask
 import socket
 import RPi.GPIO as GPIO
 import time
-import pygame
+import wave
+import pyaudio
+
+def setup():
+    for i in rnage(3):
+        GPIO.output(motor1, True)
+        time.sleep(0.5)
+        GPIO.output(motor1, False)
+        time.sleep(0.5)
+
+# 오디오 파일 재생 함수
+def play_audio(file_path):
+    wf = wave.open(file_path, 'rb')
+    p = pyaudio.PyAudio()
+    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                    channels=wf.getnchannels(),
+                    rate=wf.getframerate(),
+                    output=True)
+    
+    data = wf.readframes(1024)
+    while len(data) > 0:
+        stream.write(data)
+        data = wf.readframes(1024)
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
 GPIO.setwarnings(False)
-      
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    GPIO.output(motor1, False)
-    GPIO.output(motor2, True)
-    time.sleep(2)
-    GPIO.output(motor1, False)
-    GPIO.output(motor2, False)
     return "hello"
 
-@app.route('/vibration')
-def vibration():
-    return "vibration"
-
-@app.route('/speaker')
-def speaker():
-    pygame.mixer.music.play()
-    while pygame.mixer.music.get_busy() == True:
-        continue
-    return "speaker"
-           
-
+@app.route('/wakeup')
+def wakeup():
+    global handler
+    GPIO.output(motor1, True)
+    play_audio("/home/raspberrypi/Junee/ALERT.wav")
+    GPIO.output(motor1, False)
+    return "wakeup"
+    
 if __name__ == '__main__':
     GPIO.setmode(GPIO.BCM)
-    motor1 = 14
-    motor2 = 15
-    
+    motor1 = 26
     GPIO.setup(motor1, GPIO.OUT)
-    GPIO.setup(motor2, GPIO.OUT)
-    
-    pygame.mixer.init()
-    pygame.mixer.music.load("/home/raspberrypi/Junee/ALERT.wav")
-    app.run(host = "192.168.211.38", port = 9999, debug = True)
+
+    app.run(host = "192.168.211.38", port = 9999)
+        
